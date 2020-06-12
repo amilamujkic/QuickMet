10ffd732014046b6a8365beab37e129e32420044
amilamjk <amilamjk@gmail.com>
2020-06-12 19:16:36 +0200

    login/registration

---
+++ middleware/users.js
@@ -0,0 +1,41 @@
+module.exports = {
+    validateRegister: (req, res, next) => {
+      if (!req.body.FirstName) {
+        return res.status(400).send({
+          msg: 'Please enter your first name.'
+        });
+      }
+      if (!req.body.Surname) {
+        return res.status(400).send({
+          msg: 'Please enter your first name.'
+        });
+      }
+      if (!req.body.EmailAddress) {
+        return res.status(400).send({
+          msg: 'Please enter your first name.'
+        });
+      }
+      // password min 6 chars
+      if (!req.body.Password || req.body.Password.length < 6) {
+        return res.status(400).send({
+          msg: 'Please enter a password with min. 6 chars'
+        });
+      }
+      next();
+    },
+    isLoggedIn: (req, res, next) => {
+      try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(
+          token,
+          'SECRETKEY'
+        );
+        req.userData = decoded;
+        next();
+      } catch (err) {
+        return res.status(401).send({
+          msg: 'Your session is not valid!'
+        });
+      }
+    }
+  };
--- routes/router.js
+++ routes/router.js
@@ -7,11 +7,97 @@ const db = require('../lib/db.js');
 const userMiddleware = require('../middleware/users.js');
 
 
-router.post('/signup', (req, res, next) => {});
+router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE LOWER(EmailAddress) = LOWER(${db.escape(
+      req.body.EmailAddress
+    )});`,
+    (err, result) => {
+      if (result.length) {
+        return res.status(409).send({
+          msg: 'This email address is already in use!'
+        });
+      } else {
+        bcrypt.hash(req.body.Password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).send({
+              msg: err
+            });
+          } else {
+            db.query(
+              `INSERT INTO users (id, FirstName, Surname, EmailAddress, Password) VALUES ('${uuid.v4()}', ${db.escape(
+                req.body.EmailAddress
+              )}, ${db.escape(hash)}, now())`,
+              (err, result) => {
+                if (err) {
+                  throw err;
+                  return res.status(400).send({
+                    msg: err
+                  });
+                }
+                return res.status(201).send({
+                  msg: 'Registered!'
+                });
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+});
 
-router.post('/login', (req, res, next) => {});
+router.post('/login', (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE EmailAddress = ${db.escape(req.body.EmailAddress)};`,
+    (err, result) => {
+      if (err) {
+        throw err;
+        return res.status(400).send({
+          msg: err
+        });
+      }
+      if (!result.length) {
+        return res.status(401).send({
+          msg: 'Username or password is incorrect!'
+        });
+      }
+      bcrypt.compare(
+        req.body.Password,
+        result[0]['Password'],
+        (bErr, bResult) => {
+          if (bErr) {
+            throw bErr;
+            return res.status(401).send({
+              msg: 'Username or password is incorrect!'
+            });
+          }
+          if (bResult) {
+            const token = jwt.sign({
+                EmailAddress: result[0].EmailAddress,
+                UserId: result[0].UserId
+              },
+              'SECRETKEY', {
+                expiresIn: '7d'
+              }
+            );
+            return res.status(200).send({
+              msg: 'Logged in!',
+              token,
+              user: result[0]
+            });
+          }
+          return res.status(401).send({
+            msg: 'Username or password is incorrect!'
+          });
+        }
+      );
+    }
+  );
+});
 
-router.get('/secretroute', (req, res, next) => {
+router.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
+  console.log(req.userData);
   res.send('This is the secret content. Only logged in users can see that!');
 });
 
